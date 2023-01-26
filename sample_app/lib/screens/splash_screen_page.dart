e572974c373ebbf27a4a1acbfa0d0bcbcbe184fc
dwkim <dwkim@dsti.co.kr>
2023-01-25 21:26:42 -0800

    김도원 #SplashScreen
    1. 로고 이미지 파일명 수정

--- sample_app/lib/screens/splash_screen_page.dart
+++ sample_app/lib/screens/splash_screen_page.dart
@@ -29,7 +29,8 @@ class _SplashScreenState extends State<SplashScreen> {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-            image: AssetImage('assets/images/NikeLogo.png'), fit: BoxFit.contain),
+            image: AssetImage('assets/images/logo.png'),
+            fit: BoxFit.contain),
       ),
       child: Stack(
         children: <Widget>[
